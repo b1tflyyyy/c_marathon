79bd62ad87f18cb9312a05b3bcc2d30b3d094313
b1tfly <thisisandrew0110@gmail.com>
2023-04-18 22:21:35 +0300

    c00_ex01

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void ft_print_alphabet(void)
+{
+	for(int i = 97; i < 123; ++i)
+	{
+		printf("%c", (char)i);
+	}
+}
+
+// int main(void)
+// {
+	// ft_print_alphabet();
+	
+	// return 0;
+// }
