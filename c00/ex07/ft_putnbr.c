4075e596535d5143e8080c77ba5c2a6812efacf4
b1tfly <thisisandrew0110@gmail.com>
2023-04-21 15:18:31 +0300

    fixed bug

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,19 +6,25 @@ void ft_putnbr(int nb)
 	{
 		return;
 	}
+	else if(nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
 	
 	int temp = { nb };
-	int ascii_code[] = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
+	int symbol = { 0 };
 	
 	nb %= 10;
-
 	ft_putnbr((temp - nb) / 10);
-	write(1, &ascii_code[nb], 1);
+	
+	symbol = 48 + nb;
+	write(1, &symbol, 1);
 }
 
-int main(void)
-{
-	ft_putnbr(6450);
+// int main(void)
+// {
+	// ft_putnbr(-3421);
 	
-	return 0;
-}
+	// return 0;
+// }
