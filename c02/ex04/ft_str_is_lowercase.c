5439a5ef040d77316095934eaa7d60dc152ddc8c
b1tfly <thisisandrew0110@gmail.com>
2023-04-27 22:24:48 +0300

    c02_ex04

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// if lowercase return 0
+int ft_str_is_lowercase(char* str)
+{
+	if(*str == NULL)
+	{
+		return 1;
+	}
+	
+	int i = 0;
+	for(; *(str + i); ++i)
+	{
+		if((int)*(str + i) < 97 || (int)*(str + i) > 122)
+		{
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
+// int main(void)
+// {
+	// char str[] = "hellowolrdD";
+	// int prd = ft_str_is_lowercase(str);
+	
+	// printf("%d\n", prd);
+	
+	// return 0;
+// }
