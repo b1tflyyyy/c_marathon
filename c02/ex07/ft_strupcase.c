284784e4442929bd694abb59c2bb3b5c1f370546
b1tfly <thisisandrew0110@gmail.com>
2023-04-29 14:36:57 +0300

    c02_ex07

---
+++ c02/ex07/ft_strupcase.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// -32
+char* ft_strupcase(char* str)
+{
+	int i = 0;
+	for(; *(str + i); ++i)
+	{
+		if((int)(*(str + i)) < 65 ||
+		   (int)(*(str + i)) > 90 &&
+		   (int)(*(str + i)) < 97 ||
+		   (int)(*(str + i)) > 122)
+		   {
+			   continue;
+		   }
+		
+		*(str + i) = ((int)*(str + i)) - 32;
+	}
+	
+	return str;
+}
+
+// int main(void)
+// {
+	// char str[] = "hello world tt";
+	// ft_strupcase(str);
+	
+	// printf("%s", str);
+	
+	// return 0;
+// }
