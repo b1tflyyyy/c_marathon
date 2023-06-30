fad15530f7aa5bcdf782fcb9250a6295b85ea3d8
Myxa <thisisandrew0110@gmail.com>
2023-06-30 14:50:50 -0500

    c04_ex02

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void ft_putnbr(int nb)
+{
+	if (nb == 0)
+	{
+		return;
+	}
+	else if(nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
+	
+	int temp = { nb };
+	int symbol = { 0 };
+	
+	nb %= 10;
+	ft_putnbr((temp - nb) / 10);
+	
+	symbol = 48 + nb;
+	write(1, &symbol, 1);
+}
+
+// int main(void)
+// {
+// 	ft_putnbr(3421);
+	
+// 	return 0;
+// }
