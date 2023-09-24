28d0c91a160bfec0afc208796f3b143840e849ca
Myxa <thisisandrew0110@gmail.com>
2023-09-24 12:56:45 -0500

    c07_ex01

---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int* ft_range(int min, int max)
+{
+    if (min >= max)
+    {
+        return (void*)0;
+    }
+
+    const size_t size = max - min;
+    int* new_array = malloc(sizeof(int) * size);
+
+    for (size_t i = 0; i <= size && min <= max; ++min, ++i)
+    {
+        new_array[i] = min;        
+    }
+
+    return new_array;
+}
+
+// int main(void)
+// {
+//     int* result = ft_range(127, 150);
+
+//     for (size_t i = 0; i < 24; ++i)
+//     {
+//         printf("%d\n", result[i]);
+//     }
+
+//     return 0;
+// }
